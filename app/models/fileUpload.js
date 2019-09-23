53ce8dbd7c5b838137d1f4e30e45adc80f69206f
nancyho629 <nancyho629@gmail.com>
2019-09-23 16:28:56 -0400

    Create file upload models and routes
    Create FileUpload mongoose model to store name, type, and url of file
    Copy examples routes file and rename 'examples' to 'fileuploads' etc
      Use multer to accept files into POST routes
      FileUploadApi file to upload Buffer to S3
    Tell server.js to use the fileUpload routes

---
+++ app/models/fileUpload.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const fileUploadSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  fileType: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const FileUpload = mongoose.model('FileUpload', fileUploadSchema)
+
+module.exports = FileUpload
